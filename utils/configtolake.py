a174879eaeacccea42b61bc4a2ba690cdee1543d
Ha Vu Long <63302037+longhv19@users.noreply.github.com>
2022-03-01 10:32:18 +0700

    up load campaign.csv to lake

---
+++ utils/configtolake.py
@@ -0,0 +1,16 @@
+import pyspark
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession, Window
+
+if __name__ == '__main__':
+    spark = SparkSession.builder.master("local[1]").appName("finalProject").getOrCreate()
+    
+    source = "../PysparkMongoAirflow/Final_Project/data/"
+
+    config = spark.read.option("header", True).option("inferSchema", True).option("delimiter", '\t') \
+                        .csv(source + "source/configs/*.csv").cache()
+    
+
+    promotion.write.parquet(source+"datalake/configs")
+   
